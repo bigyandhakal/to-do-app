51d9721ceca7ec502901b09c5be7a07a86c8cd5e
Bigyan Dhakal <dhakalbigyan0@gmail.com>
2023-09-10 00:13:19 +0545

    server side and client side changes in todos

--- server/index.js
+++ server/index.js
@@ -4,14 +4,17 @@ const app = express();
 const mongoose = require("mongoose");
 const indexRouter = require('./routes');
 const PORT = +process.env.PORT || 5000;
-
+const cors = require("cors")
 
 mongoose.connect(process.env.DB_URL).then(()=>{
     console.log("Database connected")
 })
 
+app.use(cors());
+
 app.use(express.json());
 app.use("/", indexRouter);
+
 
 app.use((err, req, res, next)=>{
     err = err ? err.toString() : "Something went wrong";
