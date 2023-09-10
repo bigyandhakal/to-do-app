218571f85e04e68950be96f26f263c13551544a7
Bigyan Dhakal <dhakalbigyan0@gmail.com>
2023-09-10 22:30:48 +0545

    working with array

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
 import { useState } from "react";
 
 const App = () => {
-  const [person, setPerson] = useState({
-    name: "Bigyan",
-    sex: "Male",
-    dob: "1999",
-  });
+  const [persons, setPersons] = useState([
+    {
+      name: "Bigyan",
+      sex: "Male",
+      dob: "2001",
+    },
+    {
+      name: "Aakriti",
+      sex: "Female",
+      dob: "2004",
+    },
+  ]);
+
+  const handleChange = (index, name) => {
+    const newArr = [...persons];
+    newArr[index] = { ...newArr[index], name: name };
+    setPersons(newArr);
+  };
   return (
     <>
-      I am {person?.name}, my gender is {person?.sex} and my date of birth is{" "}
+      {/* I am {person?.name}, my gender is {person?.sex} and my age is{" "}
       {2023 - person?.dob}
       <hr />
       <input
@@ -39,7 +52,27 @@ const App = () => {
             return { ...person, dob: event.target.value };
           })
         }
-      />
+      /> */}
+
+      {persons && persons.length > 0 ? (
+        persons.map((person, index) => {
+          return (
+            <div key={index}>
+              <input
+                onChange={(event) => handleChange(index, event.target.value)}
+              />
+              <div>
+                <p>{person?.name}</p>
+                <p>{person?.sex}</p>
+                <p>{person?.dob}</p>
+                <hr />
+              </div>
+            </div>
+          );
+        })
+      ) : (
+        <>NO data</>
+      )}
     </>
   );
 };
