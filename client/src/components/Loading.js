d7c7d5edafef5147482a2500ef819973248fc9e5
Bigyan Dhakal <dhakalbigyan0@gmail.com>
2023-09-16 21:46:52 +0545

    minor changes in design

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -5,7 +5,7 @@ import 'react-loading-skeleton/dist/skeleton.css'
 export default function Loading({count}) {
   return (
     <>
-      <Skeleton count={count} height={40} />
+      <Skeleton count={count} height={35} />
     </>
   );
 }
