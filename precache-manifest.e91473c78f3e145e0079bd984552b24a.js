a73d4ddf10c1b0c82a4f994cd48ceb7b876e3bea
Bigyan Dhakal <dhakalbigyan0@gmail.com>
2023-09-19 15:46:02 +0545

    Updates

---
+++ precache-manifest.e91473c78f3e145e0079bd984552b24a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8afe398b610bd499f35",
+    "url": "/to-do-app/static/js/main.f8afe398.chunk.js"
+  },
+  {
+    "revision": "aaa53cfcb4e95a4b1d07",
+    "url": "/to-do-app/static/css/1.95c003dc.chunk.css"
+  },
+  {
+    "revision": "aaa53cfcb4e95a4b1d07",
+    "url": "/to-do-app/static/js/1.aaa53cfc.chunk.js"
+  },
+  {
+    "revision": "60e6816a297c28fee7c8",
+    "url": "/to-do-app/static/js/runtime~main.60e6816a.js"
+  },
+  {
+    "revision": "e2742d4a86ba64635793dd599cbc126d",
+    "url": "/to-do-app/index.html"
+  }
+];
